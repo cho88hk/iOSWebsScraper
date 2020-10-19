1ad00ffd2d75265dd16be273430c20340c76597a
cho <choprogram@gmail.com>
2020-10-19 19:26:09 +0800

    init

---
+++ WebsScraper/WebsScraper.h
@@ -0,0 +1,19 @@
+//
+//  WebsScraper.h
+//  WebsScraper
+//
+//  Created by leung on 19/10/2020.
+//  Copyright © 2020 cho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebsScraper.
+FOUNDATION_EXPORT double WebsScraperVersionNumber;
+
+//! Project version string for WebsScraper.
+FOUNDATION_EXPORT const unsigned char WebsScraperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebsScraper/PublicHeader.h>
+
+
